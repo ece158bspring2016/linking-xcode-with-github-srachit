b3d3d2b03f98d7c703079bcd93ac30bf647bbffc
Rachit Sengupta <srachit@gmail.com>
2016-04-07 11:36:10 -0700

    Initial Commit

---
+++ hw1_ece158/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw1_ece158
+//
+//  Created by Rachit Sengupta on 4/7/16.
+//  Copyright © 2016 Sengupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
